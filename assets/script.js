6d04a04963d71c53c7190ed9c5b5ecfe87807c88
tmn0 <attmn0@gmail.com>
2023-07-15 14:55:28 +0200

    Wip 100%

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,8 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
     
 	}
-]
+];
+const imgFolder = './assets/images/slideshow/';
 
 
 
@@ -36,11 +37,14 @@ function show_alert() {
 */
 
 
+let currentIndex = 0;
+const tagLineContainer = document.createElement("div");
+
 
 
 // Dots
 
-function Dots () {
+function CreateDots () {
   const banner = document.querySelector('#banner');
   const bullets = document.querySelectorAll('.dot');
 
@@ -54,7 +58,7 @@ function Dots () {
     document.querySelector('.dots').children[0].classList.add('dot_selected');
   });
 }
-Dots();
+CreateDots();
 
 
   
@@ -63,19 +67,30 @@ Dots();
     bullet.addEventListener('click', () => {
       
       const activeBullet = document.querySelector('.dot_selected');
-      activeBullet.classList.remove('active');
-      bullet.classList.add('active');
+      activeBullet.classList.remove('dot_selected');
+      bullet.classList.add('dot_selected');
       });
       
   });*/
 
 
+function changeBannerImg(imgPath) {
+  document.querySelector('.banner-img').setAttribute('src', imgPath);
+}
 
-const imgFolder = './assets/images/slideshow/';
+function changeTagLine(text) {
+  tagLineContainer.innerHTML = `
+    <p class="tag-line">${text}</p>;    
+  `
+}
+
 const bannerImg = document.querySelector('.banner-img');
 
+
 function InitialCarouselImg() {  
-  bannerImg.setAttribute('src', imgFolder + slides[0].image);  
+  changeBannerImg(imgFolder + slides[0].image);
+  changeTagLine(slides[0].tagLine);
+  document.querySelector('#tagLineContainer').appendChild(tagLineContainer);
 }
 InitialCarouselImg();
 
@@ -83,22 +98,86 @@ InitialCarouselImg();
 
 
 
-const nextButton = Arrow_Right;
+function changeCurrentBullet(currentIndex, nextIndex) {
+  const bullets = document.querySelector('.dots').children;
+
+  bullets[currentIndex].classList.remove("dot_selected");
+  bullets[nextIndex].classList.add("dot_selected");
+}
 
 
-nextButton.addEventListener('click', e => { 
-  bannerImg.setAttribute('src', imgFolder + slides(+1).image);
+Arrow_Right.addEventListener("click", function () {
+  console.log(currentIndex);
+
+  if (currentIndex >= slides.length - 1) {
+    currentIndex = 0;
+    changeCurrentBullet(slides.length - 1, 0);
+  } else {
+    currentIndex++;
+    changeCurrentBullet(currentIndex - 1, currentIndex);
+  }
+
+  changeBannerImg(imgFolder + slides[currentIndex].image);
+  changeTagLine(slides[currentIndex].tagLine);
+
+/*
+  slides.forEach((slide, index) => {
   
-})
+  let slideElement = document.createElement('div');
+*/
+  //bannerImg.innerHTML = 
+  //` <img src="${slides[currentIndex].image}" alt="banner image ${currentIndex}">
+  //  <p class="tag-line">${slides.tagLine}</p> `;
+  /* >>>>> Comment faire correspondre currentIndex avec par ex. img src="${slides.image} ? */
+/*
+  activeDot.classList.remove('dot_selected');
+  let nextDot = classList.nextSibling;
+  nextdot.classList.add('dot_selected');
+  */
+  /* >>>>> gérer les cas particuliers aux loops pour les dots*/
+});
+
+
+Arrow_Left.addEventListener("click", function () {
+  console.log(currentIndex);
+
+  if (currentIndex <= 0) {
+    currentIndex = slides.length - 1;
+    changeCurrentBullet(0, currentIndex);
+  } else {
+    currentIndex--;
+    changeCurrentBullet(currentIndex + 1, currentIndex);
+  }
+
+  changeBannerImg(imgFolder + slides[currentIndex].image);
+  changeTagLine(slides[currentIndex].tagLine);
+});
 
 
 
 
 
+/*
+function NextButton () {
+  Arrow_Right.addEventListener('click' , abc => {
+    const dotList = Array.from(document.querySelector('.dots').children)
+    const currentIndex = dotList.findIndex((dot, index) => {dot.classList.contains('dot_selected')});  
+
+    slides.forEach((slide, index) => {
+  const slideElement = document.createElement('div');
+  slideElement.classList.add('slide');
+  slideElement.innerHTML = `
+    <img src="${slide.image}" alt="banner image ${index ++}">
+    <p class="tag-line">${slide.tagLine}</p>;    
+  `});
+
+  })
+}
 
 
 
-var CurrentIndex
+/*
+var currentIndex
   Arrow_Left.addEventListener('click' , abc => {
     const dotList = Array.from(document.querySelector('.dots').children)
     const currentIndex = dotList.findIndex((dot, index) => {dot.classList.contains('dot_selected')});  
@@ -107,11 +186,51 @@ var CurrentIndex
   const slideElement = document.createElement('div');
   slideElement.classList.add('slide');
   slideElement.innerHTML = `
-    <img src="${slide.image}" alt="banner image ${index + 1}">
+    <img src="${slide.image}" alt="banner image ${index ++}">
     <p class="tag-line">${slide.tagLine}</p>
   `});
   
   })
+*/
+
+/* 1) Initialiser à 0 le currentIndex*
+   2) "" fleche de droite (const Arrow_Right)
+/  3) les conditions (<>)
+
+
+
+
+
+const nextButton = Arrow_Right;
+
+function nextButtonClic () {
+
+  nextButton.addEventListener('click', e => { 
+
+  bannerImg.setAttribute('src', imgFolder + slides[0].image);
+  activeDot.classList.remove('dot_selected');
+  let nextDot = e.target.closest('dot');
+  nextdot.classList.add('dot_selected');
+  
+  })
+}
+nextButtonClic();
+
+/* reformule ligne 114, concaténer i pour index
+
+
+const prevButton = Arrow_Left;
+
+prevButton.addEventListener('click', e => { 
+
+  bannerImg.setAttribute('src', imgFolder + slides(-1).image);
+  activeDot.classList.remove('dot_selected');
+  let nextDot = classList.previousSibling;
+  nextdot.classList.add('dot_selected');
+  
+})
+
+
 
 
 
@@ -127,7 +246,7 @@ function nextButtonLoop () {
 
 function prevButtonLoop () {
   if (currentIndex <= -1);
-  InitCarouselImg ();
+  
 }
  
 
@@ -147,7 +266,6 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
@@ -208,6 +326,65 @@ bullets.forEach((bullet, index) => {
 
     // Slide to the selected slide
     slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+  });
+});
+
+*/
+
+
+
+
+
+
+
+/*
+"use strict";
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+  // check if current slide is the last and reset current slide
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  //   move slide by -100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+// select next slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
 
